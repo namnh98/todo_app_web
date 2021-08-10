13a85194df45531448fd5aeb00a82a6921024ebc
Nam Nguyen <majikboy98@gmail.com>
2021-08-10 21:38:00 +0700

    update

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -9,24 +9,25 @@ function TodoForm(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    //tạo id random
+
     props.onSubmit({
       id: Math.floor(Math.random() * 10000),
       task: input,
     });
+
     setInput("");
   };
 
   return (
-    <form className="todo_form" onSubmit={handleSubmit}>
+    <form className="todo-form" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Thêm một công việc mới"
         value={input}
-        className="todo_input"
+        className="todo-input"
         onChange={handleChange}
       />
-      <button className="todo_add">Thêm</button>
+      <button className="todo-add">Thêm</button>
     </form>
   );
 }
